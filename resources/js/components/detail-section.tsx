ec59d7efe098df79b9535371cb1874f9ade1e71b
nekoLin <krusiawanbudi1@gmail.com>
2025-08-20 20:16:43 +0800

    update ui

--- resources/js/components/detail-section.tsx
+++ resources/js/components/detail-section.tsx
@@ -1,38 +1,38 @@
-import React, { forwardRef } from 'react';
-import { motion } from 'framer-motion';
-import Mandala from '@/components/mandala';
+import React from 'react';
+import { motion, MotionValue, Variants } from 'framer-motion';
 import { MapPin } from 'lucide-react';
 
+interface DetailSectionProps {
+    smoothScale?: MotionValue<number>;
+    isMochi?: boolean;
+    staggerVariants?: Variants;
+    childVariants?: Variants;
+}
 
-const MotionMandala = motion.create(Mandala);
-
-const DetailSection = forwardRef<HTMLElement>((props, ref) => {
-    const mochiPath = window.location.pathname;
-    const isMochi = mochiPath === '/mepandes-mochi';
-    
+function DetailSection({
+    isMochi,
+    staggerVariants,
+    childVariants,
+} : DetailSectionProps) {
     return (
-        <section ref={ref}  className='flex h-screen snap-start relative py-8
-        lg:flex-row-reverse lg:w-full lg:overflow-hidden'>
-            <div className='absolute lg:relative top-0 inset-x-0 flex justify-center lg:w-1/4 lg:items-center'>
-                <MotionMandala
-                    className="
-                        scale-75 md:scale-40 lg:scale-300
-                        translate-y-[-50%] lg:translate-y-0 lg:translate-x-[95%]"
-                    
-                    animate={{ rotate: 360 }}
-                    transition={{ 
-                        duration: 30,
-                        ease: "linear",
-                        repeat: Infinity
-                    }}
-                />
-            </div>
-
-            <div className='flex flex-col items-center justify-between h-3/4 relative top-1/4 gap-4 w-full
-            md:justify-evenly
-            lg:top-0 lg:w-3/4 lg:h-screen lg:justify-center lg:gap-10'>
-                <div className='flex flex-col items-center text-center font-cursive text-gold capitalize
-                '>
+        <motion.section className='
+            py-8 lg:overflow-hidden
+            flex flex-col items-center justify-center relative gap-12 w-full
+            h-screen lg:justify-center lg:items-center lg:gap-10 lg:flex-row'
+            variants={staggerVariants}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true, amount: 0.5 }}
+        >
+            {/* info */}
+            <div className='
+                flex flex-col w-full gap-12 lg:justify-center lg:items-center'>
+                <motion.div
+                    className='
+                        flex flex-col items-center text-center font-cursive text-gold capitalize
+                    '
+                    variants={childVariants}
+                >
                     { isMochi ? (
                         <span className='flex flex-col gap-3'>
                             <span className='text-3xl md:text-5xl lg:text-[4rem] text-light-gold'>your beloved hubby</span>
@@ -45,18 +45,15 @@ const DetailSection = forwardRef<HTMLElement>((props, ref) => {
                             <span className='text-5xl md:text-7xl lg:text-[6rem]'>ni made krusita budi</span>
                         </span>
                     )}
-                </div>
+                </motion.div>
 
-                <div className='lg:flex lg:flex-col-reverse lg:justify-center lg:items-center lg:gap-8 lg:w-full'>
-                    <iframe
-                        title='Lokasi Mepandes'
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d246.5249838372115!2d115.19046730161818!3d-8.653477934656491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd240aae9a1ad53%3A0x2d5d41e26ca0d464!2sGg.%20IV%20A%20No.1%2C%20Padangsambian%2C%20Kec.%20Denpasar%20Bar.%2C%20Kota%20Denpasar%2C%20Bali%2080117!5e0!3m2!1sen!2sid!4v1755188630656!5m2!1sen!2sid"
-                        className='hidden lg:block rounded-lg w-3/4 h-[240px]'
-                        loading="lazy"
-                        allowFullScreen
-                        referrerPolicy="no-referrer-when-downgrade"></iframe>
-
-                    <div className='lg:flex lg:flex-col lg:gap-3 lg:items-center'>
+                <motion.div
+                    className='
+                        flex flex-col justify-center items-center gap-8 w-full
+                    '
+                    variants={childVariants}
+                >
+                    <div className='flex flex-col gap-3 items-center'>
                         <div className='flex gap-4 items-center font-secondary text-light-gold text-2xl
                         lg:text-3xl
                         '>
@@ -68,22 +65,52 @@ const DetailSection = forwardRef<HTMLElement>((props, ref) => {
                             <span className='text-2xl text-light-gold font-secondary tracking-widest'>13.00 wita - selesai</span>
                         </div>
                     </div>
-                </div>
-                
-                <div className='lg:hidden'>
-                    <a
-                        href="https://maps.app.goo.gl/nkcdCDbcR2YoCpE79"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="mt-4 inline-flex items-center justify-center px-6 py-3 bg-gold text-black rounded-lg"
-                    >
-                        <MapPin className="mr-2 h-5 w-5" />
-                        Buka Peta Lokasi
-                    </a>
-                </div>
+                </motion.div>
             </div>
-        </section>
+
+            {/* iframe map desktop */}
+            <motion.div
+                className='hidden
+                    lg:w-3/4 lg:h-3/4
+                    lg:flex lg:flex-col lg:justify-center lg:items-center
+                    lg:px-5 lg:gap-4'
+                variants={childVariants}
+            >
+                <iframe
+                    title='Lokasi Mepandes'
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d246.5249838372115!2d115.19046730161818!3d-8.653477934656491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd240aae9a1ad53%3A0x2d5d41e26ca0d464!2sGg.%20IV%20A%20No.1%2C%20Padangsambian%2C%20Kec.%20Denpasar%20Bar.%2C%20Kota%20Denpasar%2C%20Bali%2080117!5e0!3m2!1sen!2sid!4v1755188630656!5m2!1sen!2sid"
+                    className='hidden lg:block rounded-lg w-3/4 h-1/2'
+                    loading="lazy"
+                    allowFullScreen
+                    referrerPolicy="no-referrer-when-downgrade"></iframe>
+                <a
+                    href="https://maps.app.goo.gl/nkcdCDbcR2YoCpE79"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="mt-4 w-3/4 inline-flex items-center justify-center px-6 py-3 bg-gold text-black rounded-lg"
+                >
+                    <MapPin className="mr-2 h-5 w-5" />
+                    Buka Peta Lokasi
+                </a>
+            </motion.div>
+            
+            {/* btn map mobile */}
+            <motion.div
+                className='lg:hidden'
+                variants={childVariants}
+            >
+                <a
+                    href="https://maps.app.goo.gl/nkcdCDbcR2YoCpE79"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="mt-4 inline-flex items-center justify-center px-6 py-3 bg-gold text-black rounded-lg"
+                >
+                    <MapPin className="mr-2 h-5 w-5" />
+                    Buka Peta Lokasi
+                </a>
+            </motion.div>
+        </motion.section>
     )
-});
+};
 
 export default DetailSection
--- resources/js/pages/mepandes/index.tsx
+++ resources/js/pages/mepandes/index.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { Suspense, useEffect, useRef, useState } from 'react'
 import Mandala from '@/components/mandala';
 import MandalaAtas from '@/components/mandala-atas';
 import MandalaBawah from '@/components/mandala-bawah';
-import { ArrowUp, MapPin } from 'lucide-react';
+import { ArrowUp } from 'lucide-react';
 import { motion, MotionValue, useScroll, useSpring, useTransform } from 'motion/react';
 
 function SmoothValue(value: MotionValue<number>): MotionValue<number> {
@@ -11,6 +11,8 @@ function SmoothValue(value: MotionValue<number>): MotionValue<number> {
         stiffness: 400,
     });
 }
+
+const DetailSection = React.lazy(() => import('@/components/detail-section'));
 
 function Index() {
     const containerRef = useRef<HTMLDivElement>(null);
@@ -22,10 +24,11 @@ function Index() {
 
     const { scrollYProgress } = useScroll({
         container: containerRef,
+        target: topSectionRef,
+        offset: ["start start", "end start"],
     });
 
-    const scale = useTransform(scrollYProgress, [0.1, 1], [2.25, 1.25]);
-    const smoothTranslateY = SmoothValue(useTransform(scrollYProgress, [0.1, 1], []));
+    const scale = useTransform(scrollYProgress, [0, 1], [1.85, .75]);
     const smoothScale = SmoothValue(scale);
 
     const staggerVariants = {
@@ -35,7 +38,7 @@ function Index() {
             opacity: 1,
             transition: {
                 // Atur jeda waktu antara animasi setiap anak
-                staggerChildren: 0.3,
+                staggerChildren: 0.2,
             },
         },
     };
@@ -61,20 +64,27 @@ function Index() {
     const mochiPath = window.location.pathname;
     const isMochi = mochiPath === '/mepandes-mochi';
 
-    console.log('isMochi', isMochi);
-    console.log('url', mochiPath);
+    // console.log('isMochi', isMochi);
+    // console.log('url', mochiPath);
 
     return (
-        <div ref={containerRef} className='w-full h-screen px-4 lg:px-0 bg-background overflow-y-scroll relative overflow-x-hidden'>
+        <div ref={containerRef}
+            className='
+                w-full h-svh
+                px-4 lg:px-0 pb-[env(safe-area-inset-bottom)]
+                bg-background
+                overflow-y-scroll overflow-x-hidden
+                relative'
+        >
             <section ref={topSectionRef}
                 className='flex flex-col h-screen relative py-12
-                lg:flex-row lg:w-full lg:overflow-hidden'
+                lg:flex-row lg:justify-end lg:w-full lg:overflow-hidden'
             >
 
                 <div className='
-                    flex flex-col items-center gap-4 md:gap-2
+                    flex flex-col items-center gap-4 md:gap-2 lg:justify-center
                     px-3 lg:px-8 md:w-[80%]
-                    lg:w-3/f4'>
+                    lg:w-3/4'>
                     <MandalaAtas
                         className='w-full lg:w-2/3'
                     />
@@ -115,9 +125,11 @@ function Index() {
                 </div>
             </section>
 
+            {/* Tombol scroll ke atas */}
             <motion.div 
                 onClick={handleScroll} 
-                className='fixed z-10 cursor-pointer flex items-center justify-center bottom-8 right-8'
+                className='fixed z-10 cursor-pointer flex items-center justify-center bottom-8 right-8
+                lg:bottom-16 lg:right-24'
                 animate={{ 
                     opacity: isButtonVisible ? 1 : 0, 
                     y: isButtonVisible ? 0 : 20 
@@ -132,103 +144,36 @@ function Index() {
                 </span>
             </motion.div>
 
-            <section className='
-                    flex h-screen relative py-8
-                    lg:flex-row-reverse lg:w-full lg:overflow-hidden'
-                >
-                <div className='absolute lg:relative top-0 inset-x-0 flex justify-center lg:w-1/4 lg:items-center'>
-                    <motion.div 
-                        className='translate-y-[-75%] lg:translate-y-0 lg:translate-x-[95%]'
-                        style={{ 
-                            scale: smoothScale
-                        }}
-
-                        animate={{ rotate: 360 }}
-                        transition={{ 
-                            duration: 30,
-                            ease: "linear",
-                            repeat: Infinity
-                        }}
-                    >
-                        <Mandala
-                            className='w-full'/>
-                    </motion.div>
-                </div>
-
+            {/* mandala */}
+            <div
+                className='
+                    absolute bottom-0 inset-x-0 flex justify-center
+                    lg:left-0 lg:bottom-1/2 lg:translate-y-1/2 lg:translate-x-[-50%] lg:w-1/4 lg:items-center'>
                 <motion.div 
-                    className='
-                        flex flex-col items-center justify-center h-3/4 relative top-1/5 gap-12 w-full
-                        md:justify-evenly
-                        lg:top-0 lg:w-3/4 lg:h-screen lg:justify-center lg:gap-10'
-                    variants={staggerVariants}
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: true, amount: 0.5 }}
+                    className='translate-y-[50%] lg:translate-y-0'
+                    style={{ 
+                        scale: smoothScale
+                    }}
+                    animate={{ rotate: 360 }}
+                    transition={{ 
+                        duration: 30,
+                        ease: "linear",
+                        repeat: Infinity
+                    }}
                 >
-                    <motion.div
-                        className='
-                            flex flex-col items-center text-center font-cursive text-gold capitalize
-                        '
-                        variants={childVariants}
-                    >
-                        { isMochi ? (
-                            <span className='flex flex-col gap-3'>
-                                <span className='text-3xl md:text-5xl lg:text-[4rem] text-light-gold'>your beloved hubby</span>
-                                <span className='text-5xl md:text-7xl lg:text-[6rem]'>i gede krusiawan budi</span>
-                            </span>
-                        ) : (
-                            <span className='flex flex-col'>
-                                <span className='text-5xl md:text-7xl lg:text-[6rem]'>i gede krusiawan budi</span>
-                                <span className='text-8xl text-light-gold'>&</span>
-                                <span className='text-5xl md:text-7xl lg:text-[6rem]'>ni made krusita budi</span>
-                            </span>
-                        )}
-                    </motion.div>
-
-                    <motion.div
-                        className='
-                            lg:flex lg:flex-col-reverse lg:justify-center lg:items-center lg:gap-8 lg:w-full
-                        '
-                        variants={childVariants}
-                    >
-                        <iframe
-                            title='Lokasi Mepandes'
-                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d246.5249838372115!2d115.19046730161818!3d-8.653477934656491!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd240aae9a1ad53%3A0x2d5d41e26ca0d464!2sGg.%20IV%20A%20No.1%2C%20Padangsambian%2C%20Kec.%20Denpasar%20Bar.%2C%20Kota%20Denpasar%2C%20Bali%2080117!5e0!3m2!1sen!2sid!4v1755188630656!5m2!1sen!2sid"
-                            className='hidden lg:block rounded-lg w-3/4 h-[240px]'
-                            loading="lazy"
-                            allowFullScreen
-                            referrerPolicy="no-referrer-when-downgrade"></iframe>
-
-                        <div className='lg:flex lg:flex-col lg:gap-3 lg:items-center'>
-                            <div className='flex gap-4 items-center font-secondary text-light-gold text-2xl
-                            lg:text-3xl
-                            '>
-                                <span className='border-y-2 border-gold py-0.5 tracking-widest px-1'>Rabu</span>
-                                <span className='text-6xl lg:text-7xl'>27</span>
-                                <span className='border-y-2 border-gold py-0.5 tracking-widest px-1'>Agustus</span>
-                            </div>
-                            <div>
-                                <span className='text-2xl text-light-gold font-secondary tracking-widest'>13.00 wita - selesai</span>
-                            </div>
-                        </div>
-                    </motion.div>
-                    
-                    <motion.div
-                        className='lg:hidden'
-                        variants={childVariants}
-                    >
-                        <a
-                            href="https://maps.app.goo.gl/nkcdCDbcR2YoCpE79"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="mt-4 inline-flex items-center justify-center px-6 py-3 bg-gold text-black rounded-lg"
-                        >
-                            <MapPin className="mr-2 h-5 w-5" />
-                            Buka Peta Lokasi
-                        </a>
-                    </motion.div>
+                    <Mandala
+                        className='w-full'/>
                 </motion.div>
-            </section>
+            </div>
+
+            <Suspense fallback={null}>
+                <DetailSection
+                    smoothScale={smoothScale}
+                    staggerVariants={staggerVariants}
+                    childVariants={childVariants}
+                    isMochi={isMochi}
+                />
+            </Suspense>
         </div>
     )
 }
